90f062e7b66d17e599e6447225dd6bff1996b240
Fekhreddine23 <fekhredine1990@gmail.com>
2023-02-14 17:25:59 +0100

    recup des todos depuis serveur api

--- src/app/componant/todos/todos.component.ts
+++ src/app/componant/todos/todos.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/app/models/todo';
+import { TodoService } from 'src/app/services/todo.service';
 
 @Component({
   selector: 'app-todos',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent {
+
+  todos: Todo[] = [];
+  constructor(private _todoService: TodoService){}
+
+  ngOnInit(){ //on chargement appel findAll()
+    this._todoService
+    .findAll()
+    .subscribe(todosReceived =>{
+      this.todos = todosReceived;
+    });
+  }
 
 }
